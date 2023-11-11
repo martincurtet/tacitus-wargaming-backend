ea1f813a9cbe1c621eb327a5388972eb53c36487
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-11 15:37:00 -0500

    feat: socket.io
    - added a socket.io server
    - added simple connection disconnection mechanic

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const { createServer } = require('node:http')
+const socket = require('./src/socket')
 
 const URL = 'http://localhost'
 const PORT = 8080
 
 const app = express()
+const server = createServer(app)
+socket(server)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`# Server running at ${URL}:${PORT}`)
 })
---
+++ src/socket.js
@@ -0,0 +1,16 @@
+const { Server } = require('socket.io')
+
+module.exports = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000'
+    }
+  })
+
+  io.on('connection', (socket) => {
+    console.log(`# User connected`)
+    socket.on('disconnect', () => {
+      console.log(`# User disconnected`)
+    })
+  })
+}
