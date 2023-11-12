857ae5266f8819a743b4312abf884cf059b4013e
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-12 07:38:26 -0500

    feat: log
    - added create log entry function
    - added log entries when room data changes

--- src/rooms.js
+++ src/rooms.js
@@ -15,6 +15,7 @@ const createRoom = () => {
     users: [] // { id: '', username: '' }
   }
   console.log(`# Room ${uuid} created`)
+  createLog(uuid, `Room created`)
   return uuid
 }
 
@@ -28,6 +29,7 @@ const readRoom = (uuid) => {
 
 const deleteRoom = (uuid) => {
   if (rooms.hasOwnProperty(uuid)) {
+    createLog(uuid, `Room deleted`)
     delete rooms[uuid]
     console.log(`# Room ${uuid} deleted`)
   } else {
@@ -39,6 +41,7 @@ const deleteRoom = (uuid) => {
 const createUser = (uuid, userId, username) => {
   if (rooms.hasOwnProperty(uuid)) {
     rooms[uuid].users.push({ id: userId, username: username })
+    createLog(uuid, `User ${userId} joined room with username ${username}`)
   } else {
     console.error(`# Could find room ${uuid}`)
   }
@@ -49,6 +52,7 @@ const deleteUser = (uuid, userId) => {
     const userIndex = rooms[uuid].users.findIndex(u => u.id === userId)
     if (userIndex !== -1) {
       rooms[uuid].users.splice(userIndex, 1)[0]
+      createLog(uuid, `User ${userId} left room`)
     } else {
       console.error(`# Could find user ${userId}`)
     }
@@ -57,7 +61,17 @@ const deleteUser = (uuid, userId) => {
   }
 }
 
+// LOG CRUD
+const createLog = (uuid, log) => {
+  if (rooms.hasOwnProperty(uuid)) {
+    rooms[uuid].log.push({ timestamp: new Date().getTime(), log: log })
+  } else {
+    console.error(`# Could find room ${uuid}`)
+  }
+}
+
 module.exports = {
+  rooms,
   createRoom,
   readRoom,
   deleteRoom,
