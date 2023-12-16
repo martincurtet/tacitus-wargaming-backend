c34e68eb13f82bdf49bb94f44e499a5817a460fe
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-12-15 21:23:10 -0500

    feat: compare units & new units
    - added new units
    - added function to compare two units

--- src/units.js
+++ src/units.js
@@ -1,38 +1,121 @@
-// key: {
+// {
+//   code: '',
+//   hdPerMen: '',
+//   icon: '',
 //   name: '',
-//   experience: '',
-//   hd: 0,
-//   ac: 0,
-//   men: 0
+//   veterancy: ''
 // }
 
 const unitShop = [
   {
-    code: 'SPE',
-    name: 'Spearman',
+    code: 'MSPR',
+    hdPerMen: '1',
     icon: 'spearman.png',
-    hdPerMen: '2'
+    name: 'Militia Spearman',
+    veterancy: '0'
   },
   {
-    code: 'INF',
-    name: 'Infantry',
+    code: 'MBOM',
+    hdPerMen: '1',
     icon: 'infantry.png',
-    hdPerMen: '2'
+    name: 'Militia Band of Misfits',
+    veterancy: '0'
   },
   {
-    code: 'ARC',
-    name: 'Archer',
-    icon: 'archer.png',
-    hdPerMen: '1'
+    code: 'NDWLN',
+    hdPerMen: '2',
+    icon: 'infantry.png',
+    name: 'Normal Dwarfen Longbeard',
+    veterancy: '1'
   },
   {
-    code: 'CAV',
-    name: 'Cavalry',
-    icon: 'cavalry.png',
-    hdPerMen: '5'
+    code: 'NLINF',
+    hdPerMen: '2',
+    icon: 'infantry.png',
+    name: 'Normal Light Infantry',
+    veterancy: '1'
+  },
+  {
+    code: 'NJINF',
+    hdPerMen: '2',
+    icon: 'infantry.png',
+    name: 'Normal Javelin Infantry',
+    veterancy: '1'
+  },
+  {
+    code: 'VMINF',
+    hdPerMen: '3',
+    icon: 'infantry.png',
+    name: 'Veteran Medium Infantry',
+    veterancy: '2'
+  },
+  {
+    code: 'VHINF',
+    hdPerMen: '3',
+    icon: 'infantry.png',
+    name: 'Veteran Heavy Infantry',
+    veterancy: '2'
+  },
+  {
+    code: 'VPHAL',
+    hdPerMen: '3',
+    icon: 'infantry.png',
+    name: 'Veteran Phalanx',
+    veterancy: '2'
+  },
+  {
+    code: 'EHASS',
+    hdPerMen: '5',
+    icon: 'infantry.png',
+    name: 'Elite Human Assassins',
+    veterancy: '3'
+  },
+  {
+    code: 'EHINF',
+    hdPerMen: '5',
+    icon: 'infantry.png',
+    name: 'Elite Heavy Infantry',
+    veterancy: '3'
+  },
+  {
+    code: 'VPATH',
+    hdPerMen: '3',
+    icon: 'spearman.png',
+    name: 'Veteran Pathfinder',
+    veterancy: '2'
+  },
+  {
+    code: 'VDROW',
+    hdPerMen: '3',
+    icon: 'infantry.png',
+    name: 'Veteran Drow Sappers',
+    veterancy: '2'
   }
 ]
 
+const compareUnits = (unit1, unit2) => {
+  const modifiedProperties = ['hd', 'casualties', 'fatigue', 'notes']
+  let modifiedPropertyCount = 0
+  let modifiedProperty = ''
+
+  for (const prop of modifiedProperties) {
+    if (unit1[prop] !== unit2[prop]) {
+      modifiedPropertyCount++
+      modifiedProperty = prop
+    }
+  }
+
+  if (modifiedPropertyCount === 1) {
+    const originalValue = unit1[modifiedProperty]
+    const newValue = unit2[modifiedProperty]
+    return [modifiedProperty, originalValue, newValue]
+  } else {
+    return null
+  }
+}
+
+
 module.exports = {
-  unitShop
+  unitShop,
+  compareUnits
 }
